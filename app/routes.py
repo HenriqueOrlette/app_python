cde5cd96c256526d904a6cede89ce0773155af46
HenriqueOrlette <125471532+HenriqueOrlette@users.noreply.github.com>
2025-02-25 08:49:07 -0300

    Add files via upload

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,20 @@
 # app/routes.py
 
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Flamengo"
-
+    user = {'username' : 'Henrique'}
+    posts = [
+        {
+            'author': {'username' : 'Bruno'},
+            'body' : 'Gasolina tá cara'
+        },
+        {
+            'author': {'username' : 'Pedro Maia'},
+            'body' : 'Caí da cadeira'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
